36fc44999ec0ae843c1d2f7ce26f93cc07cd4a3b
Konstantinos Katris <93059645+katrhss@users.noreply.github.com>
2023-06-13 02:14:23 +0300

    Added repo links to projects and added styles

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// test
-
-// console.log(element);
-
-const x = function () {
-  console.log("test");
-};
-
 const btn = document.querySelector(".get-in-touch");
 
 const form = document.querySelector(".form");
@@ -14,22 +6,82 @@ const blur = document.getElementById("blur");
 
 const closeForm = document.querySelector(".form-close");
 
-console.log(document.title);
+const submitBtn = document.getElementById("submit-btn");
+
+console.log(submitBtn.classList);
 
 btn.addEventListener("click", () => {
+  closeForm.classList.remove("hidden");
+  submitBtn.setAttribute("hidden", "false");
   form.classList.add("display-form");
   blur.classList.add("blur");
   icons.classList.add("blur");
 });
 
-closeForm.addEventListener("mouseover", () => {
-  document
-    .getElementById(".form-close-icon")
-    .classList.add("form-close-icon-hover");
-});
-
 closeForm.addEventListener("click", () => {
+  submitBtn.setAttribute("hidden", "");
+  closeForm.classList.add("hidden");
   form.classList.remove("display-form");
   blur.classList.remove("blur");
   icons.classList.remove("blur");
+});
+
+//* Project Box 2
+
+const projectBox1 = document.getElementById("box-1");
+const projectBox1Icon = document.getElementById("box-icon-1");
+
+projectBox1.addEventListener("mouseover", () => {
+  projectBox1.classList.add("blur");
+  projectBox1Icon.classList.add("set-visible");
+});
+
+projectBox1.addEventListener("mouseout", () => {
+  projectBox1Icon.classList.remove("set-visible");
+  projectBox1.classList.remove("blur");
+});
+
+projectBox1Icon.addEventListener("mouseover", () => {
+  projectBox1.classList.add("blur");
+  projectBox1Icon.classList.add("set-visible");
+});
+
+//* Project Box 2
+
+const projectBox2 = document.getElementById("box-2");
+const projectBox2Icon = document.getElementById("box-icon-2");
+
+projectBox2.addEventListener("mouseover", () => {
+  projectBox2.classList.add("blur");
+  projectBox2Icon.classList.add("set-visible");
+});
+
+projectBox2.addEventListener("mouseout", () => {
+  projectBox2Icon.classList.remove("set-visible");
+  projectBox2.classList.remove("blur");
+});
+
+projectBox2Icon.addEventListener("mouseover", () => {
+  projectBox2.classList.add("blur");
+  projectBox2Icon.classList.add("set-visible");
+});
+
+//* Project Box 3
+
+const projectBox3 = document.getElementById("box-3");
+const projectBox3Icon = document.getElementById("box-icon-3");
+
+projectBox3.addEventListener("mouseover", () => {
+  projectBox3.classList.add("blur");
+  projectBox3Icon.classList.add("set-visible");
+});
+
+projectBox3.addEventListener("mouseout", () => {
+  projectBox3Icon.classList.remove("set-visible");
+  projectBox3.classList.remove("blur");
+});
+
+projectBox3Icon.addEventListener("mouseover", () => {
+  projectBox3.classList.add("blur");
+  projectBox3Icon.classList.add("set-visible");
 });
